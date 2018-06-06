a6a2d0693a4bf9dd1fd7b9d89c2ece006bffa8ee
sonnd9x <phamthanhnga@hlu.edu.vn>
2018-06-06 09:05:04 +0700

    Init project
    Init project

---
+++ SimpleCaptcha/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using SimpleCaptcha.Models;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SimpleCaptcha.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            Session["CAPTCHA"] = GetRandomText();
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string serverCaptcha = Session["CAPTCHA"].ToString();
+                if (!model.Captcha.Equals(serverCaptcha))
+                {
+                    ViewBag.CaptchaError = "Sorry, please write exact text as written above.";
+                    Session["CAPTCHA"] = GetRandomText();
+                    return View(model);
+                }
+                else
+                {
+                    //Validate user and password
+                    //Redirect to success page
+                }
+            }
+            return View(model);
+        }
+
+        private string GetRandomText()
+        {
+            StringBuilder randomText = new StringBuilder();
+            string alphabets = "012345679ACEFGHKLMNPRSWXZabcdefghijkhlmnopqrstuvwxyz";
+            Random r = new Random();
+            for (int j = 0; j <= 5; j++)
+            {
+                randomText.Append(alphabets[r.Next(alphabets.Length)]);
+            }
+            return randomText.ToString();
+        }
+
+        public FileResult GetCaptchaImage()
+        {
+            string text = Session["CAPTCHA"].ToString();
+
+            //first, create a dummy bitmap just to get a graphics object
+            Image img = new Bitmap(1, 1);
+            Graphics drawing = Graphics.FromImage(img);
+
+            Font font = new Font("Arial", 15);
+            //measure the string to see how big the image needs to be
+            SizeF textSize = drawing.MeasureString(text, font);
+
+            //free up the dummy image and old graphics object
+            img.Dispose();
+            drawing.Dispose();
+
+            //create a new image of the right size
+            img = new Bitmap((int)textSize.Width + 40, (int)textSize.Height + 20);
+            drawing = Graphics.FromImage(img);
+
+            Color backColor = Color.Black;
+            Color textColor = Color.White;
+            //paint the background
+            drawing.Clear(backColor);
+
+            //create a brush for the text
+            Brush textBrush = new SolidBrush(textColor);
+
+            drawing.DrawString(text, font, textBrush, 20, 10);
+
+            drawing.Save();
+
+            font.Dispose();
+            textBrush.Dispose();
+            drawing.Dispose();
+
+            MemoryStream ms = new MemoryStream();
+            img.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+            img.Dispose();
+
+            return File(ms.ToArray(), "image/png");
+        }
+    }
+}
